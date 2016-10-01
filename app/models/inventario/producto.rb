712fcd0ce7e97d55e2138f08c9f5cf56ea7ce979
Juan Evangelista Fletes Garcia <jefg85@gmail.com>
2016-10-01 04:46:34 -0600

    Agregando categoria de productos

--- app/models/inventario/producto.rb
+++ app/models/inventario/producto.rb
@@ -1,5 +1,7 @@
 class Inventario::Producto < ActiveRecord::Base
 	strip_attributes
 	has_paper_trail
-	belongs_to :rel_pedido_detalle, class_name: 'Ventas::PedidoDetalle', foreign_key: 'producto_id'
+
+	has_many :rel_pedido_detalle, class_name: 'Ventas::PedidoDetalle', foreign_key: 'producto_id'
+  belongs_to :rel_categoria_producto, class_name: 'Inventario::CategoriaProducto', foreign_key: 'categoria_producto_id'
 end
